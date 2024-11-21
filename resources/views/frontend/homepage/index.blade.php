b7f9969a5e63a324ad36ceb7564195937d3b0d88
Stefanos Georgopoulos <stefanos.georgopoulos@fau.de>
2024-11-21 14:24:42 +0100

    Add acl information to series and clip pages
    Signed-off-by: Stefanos Georgopoulos <stefanos.georgopoulos@fau.de>

--- resources/views/frontend/homepage/index.blade.php
+++ resources/views/frontend/homepage/index.blade.php
@@ -51,7 +51,7 @@
                             <a href="{{ route('frontend.series.index') }}"
                                class="inline-flex justify-center items-center py-2 px-5 text-base font-medium text-center text-white
                                       rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
-                                {{__('homepage.series.more clips') }}
+                                {{__('homepage.series.more series') }}
                                 <svg class="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true"
                                      xmlns="http://www.w3.org/2000/svg"
                                      fill="none" viewBox="0 0 14 10">
@@ -86,7 +86,7 @@
                             <a href="{{ route('frontend.series.index') }}"
                                class="inline-flex justify-center items-center py-2 px-5 text-base font-medium text-center text-white
                                       rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
-                                {{__('homepage.series.more clips') }}
+                                {{__('homepage.series.more series') }}
                                 <svg class="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true"
                                      xmlns="http://www.w3.org/2000/svg"
                                      fill="none" viewBox="0 0 14 10">
@@ -124,7 +124,8 @@
                         <svg class="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true"
                              xmlns="http://www.w3.org/2000/svg"
                              fill="none" viewBox="0 0 14 10">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                  stroke-width="2"
                                   d="M1 5h12m0 0L9 1m4 4L9 9" />
                         </svg>
                     </a>
@@ -156,7 +157,8 @@
                         <svg class="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true"
                              xmlns="http://www.w3.org/2000/svg"
                              fill="none" viewBox="0 0 14 10">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                  stroke-width="2"
                                   d="M1 5h12m0 0L9 1m4 4L9 9" />
                         </svg>
                     </a>
